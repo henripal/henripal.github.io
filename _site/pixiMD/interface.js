f7aa18b494495cb001bf53871e75e69ae4bbdacb
henripal <rrrispam@gmail.com>
2015-11-21 09:09:03 -0500

    updated submodule?

--- _site/pixiMD/interface.js
+++ _site/pixiMD/interface.js
@@ -16,7 +16,7 @@ $(function() {
 
 // Lennard Jones Potential 1
 $("#LJAslider").slider({
-    value:1, min: 0, max: 10, step: 0.5,
+    value:2, min: 0, max: 10, step: 0.5,
     slide: function( event, ui) {
         $( "#LJAslider-val" ).html(ui.value);
         updateParams();
@@ -37,7 +37,7 @@ $( "#LJBslider-val" ).html(  $('#LJBslider').slider('value') );
 
 // Diffusion Coefficient
 $("#Dslider").slider({
-            value:3000, min: 0, max: 100000,
+            value:15000, min: 0, max: 100000,
             slide: function( event, ui) {
                 $( "#Dslider-val" ).html(ui.value);
                 updateParams();
