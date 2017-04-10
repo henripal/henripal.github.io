af5ec06e37ba8e90526af0ba5fbf6e44c9f84328
henripal <henri.palacci@gmail.com>
2017-04-10 00:18:40 -0400

    canvas setup

--- game/game.js
+++ game/game.js
@@ -1,91 +1,108 @@
-
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-
 var score = 0;
 var scoreText;
+var map_x = 14;
+var map_y = 10;
+
+var game = new Phaser.Game(map_x * 16, map_y * 16, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
 
 function preload() {
-    game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
+    // game.scale.maxWidth = 600;
+    // game.scale.maxHeight = 600;
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.setScreenSize();
+    game.load.image('cat', 'assets/cat.png', 16, 16);
+    game.load.image('ground', 'assets/darkfloor.png', 16, 16);
+    game.load.image('l_wall', 'assets/l_wall.png', 16, 16);
+    game.load.image('r_wall', 'assets/r_wall.png', 16, 16);
+    game.load.image('t_wall', 'assets/t_wall.png', 16, 16);
+    game.load.image('tr_wall', 'assets/tr_wall_iso.png', 16, 16);
+    game.load.image('tl_wall', 'assets/tl_wall_iso.png', 16, 16);
+    game.load.image('bl_wall', 'assets/bl_wall.png', 16, 16);
+    game.load.image('br_wall', 'assets/br_wall.png', 16, 16);
+    game.load.image('stone_door', 'assets/door_stone.png', 16, 16);
     game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png',32, 48);
 }
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.add.sprite(0, 0, 'sky');
 
     // the game world
-    platforms = game.add.group();
-    platforms.enableBody = true;
 
-    var ground = platforms.create(0, game.world.height -64, 'ground');
-    ground.scale.setTo(2, 2);
-    ground.body.immovable = true;
+    gmap = game.add.tilemap();
+    gmap.addTilesetImage('ground', 'ground', 16, 16, null, null, 0);
+    gmap.addTilesetImage('l_wall', 'l_wall', 16, 16, null, null, 1);
+    gmap.addTilesetImage('r_wall', 'r_wall', 16, 16, null, null, 2);
+    gmap.addTilesetImage('tr_wall', 'tr_wall', 16, 16, null, null, 3);
+    gmap.addTilesetImage('tl_wall', 'tl_wall', 16, 16, null, null, 4);
+    gmap.addTilesetImage('br_wall', 'br_wall', 16, 16, null, null, 5);
+    gmap.addTilesetImage('bl_wall', 'bl_wall', 16, 16, null, null, 6);
+    gmap.addTilesetImage('t_wall', 't_wall', 16, 16, null, null, 7);
+    gmap.addTilesetImage('stone_door', 'stone_door', 16, 16, null, null, 8);
+    ground_layer = gmap.create('ground_layer', map_x, map_y, 16, 16);
+    wall_layer = gmap.create('wall_layer', map_x, map_y, 16, 16);
 
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
-    ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = true;
+    for (var i=0; i<map_x; i++) {
+        for(var j=0; j<map_y; j++) {
+            if (i==0 && j==0) {
+                gmap.putTile(4, i, j, wall_layer);
+            } else if (i==map_x/2 && j==0) {
+                gmap.putTile(8, i, j, wall_layer);
+            } else if (i==map_x-1 && j == map_y-1) {
+                gmap.putTile(5, i, j, wall_layer);
+            } else if (i==0 && j == map_y-1) {
+                gmap.putTile(6, i, j, wall_layer);
+            } else if (i==map_x-1 && j == 0) {
+                gmap.putTile(3, i, j, wall_layer);
+            } else if (i==map_x-1 && j == map_y-1) {
+                gmap.putTile(6, i, j, wall_layer);
+            } else if (i==0) {
+                gmap.putTile(1, i, j, wall_layer);
+            } else if(i==map_x-1) {
+                gmap.putTile(2, i, j, wall_layer);
+            } else if(j==map_y-1) {
+                gmap.putTile(7, i, j, wall_layer);
+            } else if(j==0) {
+                gmap.putTile(7, i, j, wall_layer);
+            } else {
+                gmap.putTile(0, i, j, ground_layer);
+            }
+        }
+    }
+
+    wall_layer.resizeWorld();
+    game.physics.arcade.enable(wall_layer);
+    gmap.setCollision(wall_layer);
+
 
     // the player
-    player = game.add.sprite(32, game.world.height - 150, 'dude')
+    player = game.add.sprite(32, 32, 'cat');
     game.physics.arcade.enable(player);
 
-    player.body.bounce.y = .2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
-
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
+     player.body.collideWorldBounds = true;
+    // gmap.setCollisionBetween(0, 100, true, wall_layer);
 
     cursors = game.input.keyboard.createCursorKeys();
 
-    // the stars
-    stars = game.add.group();
-    stars.enableBody = true;
-
-    for (var i=0; i < 12; i++) {
-        var star = stars.create(i*70, 0, 'star')
-        star.body.gravity.y = 6;
-        star.body.bounce.y = 0.7 + Math.random()*.2;
-    }
-
-    // the score
-
-    scoreText = game.add.text(16, 16, 'score: 0', {
-        fontSize: '32px', fill: '#000' });
 }
 
 function update() {
     
-    var hitPlatform = game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(stars, platforms);
-
-    // important, we pass the argument to the collectStart function!
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+    game.physics.arcade.collide(player, wall_layer);
 
     player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
+
     if (cursors.left.isDown) {
         player.body.velocity.x = -150;
-        player.animations.play('left');
     } else if (cursors.right.isDown) {
         player.body.velocity.x = 150;
-        player.animations.play('right');
+    } else if (cursors.down.isDown) {
+        player.body.velocity.y = 150;
+    } else if (cursors.up.isDown) {
+        player.body.velocity.y = -150;
     } else {
-        player.animations.stop();
-        player.frame = 4;
     }
 
-    if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
-        player.body.velocity.y = -350
-    }
-
-    
-}
-function collectStar(player, star) {
-    star.kill();
-    score += 10;
-    scoreText.text = 'Score: ' + score;
 }
 
